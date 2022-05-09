356712bd2e5fc8c22a382614c3b36c016a935cf6
Jessica Mary <jessicamary472@gmail.com>
2022-05-09 15:27:45 +0700

    Hot fixes + add typewriter effect to header

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,29 +2,41 @@
 // import Icon from './Icon';
 import {Stack} from '@mui/material';
 import profilePic from '../images/profile-picture.jpg';
+import Typewriter from 'typewriter-effect/';
 
 const Home = () => {
     // const [isLoading, setIsLoading] = useState(true);
     // const toggleLoading = () => {setIsLoading(false)};
 
   return (
-      <Stack spacing={0.5}>
-          <h1 className="calligraphy">Hello There!</h1>
+    <body>
+      <Stack spacing={3} direction="row" alignItems="stretch">
+        <div className="zoom">
+          <img className="profile-cropped" src={profilePic} alt="Jessica-Mary"></img>
+        </div>
+        <Stack spacing={1}>
+          {/* <h1 className="calligraphy">Hello There!</h1> */}
+          <Typewriter
+            options={{
+              strings: ["Hello there!"],
+              delay: 150,
+              pauseFor: 3000,
+              autoStart: true,
+              loop: true,
+              skipAddStyles: false,
+              cursorClassName: 'cursor',
+              wrapperClassName: 'typewriter-wrapper',
+            }}
+          />
           {/* TODO add icon star or wave */}
-          <Stack spacing={2} direction="row" alignItems="stretch">
-            <div className="para">
-              My name is Jessica, a <strong>Pharmaceutical Science</strong> undergraduate with a 
-              minor in <strong>Computer Science</strong> in National University of Singapore. 
-              I love creating new things, including music covers. 
-            </div>
-            <div className="zoom">
-              <img className="profile-cropped" src={profilePic} alt="Jessica-Mary"
-              width="300"></img>
-            </div>
-          </Stack>
+          <div className="para">
+            My name is Jessica, a penultimate student in National University of Singapore, 
+            majoring in <strong>Pharmaceutical Science</strong> with a minor in <strong>Computer Science</strong>. 
+            I love exploring new things and I am really passionate about music.
+          </div>
+        </Stack>
       </Stack>
-    
-
+    </body>
   );
 }
 
