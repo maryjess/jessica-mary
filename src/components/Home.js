75204b2617d996b7d8ffa004dc2a477a64a94a73
Jessica Mary <jessicamary472@gmail.com>
2022-05-09 14:18:12 +0700

    Add dashboard message and profile picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,31 @@
-import {useState} from 'react';
-import Icon from './Icon';
-import {FcMusic} from 'react-icons/fc';
+// import {useState} from 'react';
+// import Icon from './Icon';
+import {Stack} from '@mui/material';
+import profilePic from '../images/profile-picture.jpg';
 
 const Home = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const toggleLoading = () => {setIsLoading(false)};
+    // const [isLoading, setIsLoading] = useState(true);
+    // const toggleLoading = () => {setIsLoading(false)};
 
-  return ( [
-    <h1 className="calligraphy">Hello There!</h1>, // TODO add icon
+  return (
+      <Stack spacing={0.5}>
+          <h1 className="calligraphy">Hello There!</h1>
+          {/* TODO add icon star or wave */}
+          <Stack spacing={2} direction="row" alignItems="stretch">
+            <div className="para">
+              My name is Jessica, a <strong>Pharmaceutical Science</strong> undergraduate with a 
+              minor in <strong>Computer Science</strong> in National University of Singapore. 
+              I love creating new things, including music covers. 
+            </div>
+            <div className="zoom">
+              <img className="profile-cropped" src={profilePic} alt="Jessica-Mary"
+              width="300"></img>
+            </div>
+          </Stack>
+      </Stack>
+    
 
-  ]);
+  );
 }
 
 export default Home;
