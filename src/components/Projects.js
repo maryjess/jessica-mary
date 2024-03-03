043656a470e3f23118e312f166b0862d0ab4443a
Jessica Mary <jessicamary472@gmail.com>
2024-03-03 22:13:02 +0800

    Add projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,26 +1,188 @@
-import { Stack } from '@mui/material';
+import { Stack, Card, CardContent, CardMedia, CardActions, CardActionArea, Button } from '@mui/material';
+import internconnect from '../images/internconnect.png';
+import mew from '../images/mew.png';
+import qr_bookmark from '../images/qr-bookmark.jpg';
 import React from 'react';
 
 const Projects = () => {
   return (
-    <Stack spacing={1}>
+    <Stack spacing={2} justifyContent="center" alignItems="center">
       <div className="card-header__works works">
         <h1 className="card-title">Projects</h1>
       </div>  
-      <Stack direction="row" spacing={5}>
-        <div className="card-background">
-          <div className="para">
-            My name is Jessica, a penultimate student in National University of Singapore, 
-            majoring in <strong>Pharmaceutical Science</strong> with a minor in <strong>Computer Science</strong>. 
-            I love exploring new things and I am really passionate about music.
-          </div>
+      <Stack flexDirection={{xs:"column", xl:"row"}} justifyContent="center" alignItems="center" spacing={{xs: 2, xl: 0}}>
+        <div className="padding-right">
+          <Card
+            sx={{
+              width: "450px",
+              height: "450px",
+              borderRadius: "10px",
+              backgroundColor: "#efffe0",
+              display: "flex"
+            }}
+          >
+            <div className="card-contents">
+              <CardActionArea href="https://ay2223s1-cs2103-f14-2.github.io/tp/">
+                <CardMedia
+                  className="center"
+                  sx={{height: "200px", width: "450px", maxHeight: "200px", maxWidth: "450px"}}
+                  image={internconnect}
+                  title="InternConnect"
+                />
+              </CardActionArea>
+              <CardContent
+                sx={{padding: "30px", paddingBottom: "0px"}}
+              >
+                <Stack justifyContent="space-evenly">
+                  <a className="card less-spacing" href="https://ay2223s1-cs2103-f14-2.github.io/tp/">
+                    <h3 className="card less-spacing">InternConnect</h3>
+                  </a>
+                  <p className="para less-spacing">a one-stop platform to help internship
+                  campus recruiters who prefer CLI over GUI work with their applicants’ data.</p>
+                  <CardActions>
+                    <Stack direction="row" spacing={2}>
+                      <Button 
+                        id="card-button"
+                        href="https://ay2223s1-cs2103-f14-2.github.io/tp/"
+                        sx={{fontFamily: "Montserrat", textTransform: "none"}} 
+                        variant="contained" 
+                        color="success" 
+                        size="small"
+                      >
+                        Learn More
+                      </Button>
+                      <Button 
+                        id="card-button"
+                        href="https://github.com/AY2223S1-CS2103-F14-2/tp"
+                        sx={{fontFamily: "Montserrat", textTransform: "none"}} 
+                        color="success" 
+                        variant="outlined"
+                        size="small"
+                      >
+                        Source Code
+                      </Button>
+                    </Stack>
+                  </CardActions>
+                </Stack>
+              </CardContent>
+            </div>
+          </Card>
         </div>
-        <div className="card-background">
-          <div className="para">
-            My name is Jessica, a penultimate student in National University of Singapore, 
-            majoring in <strong>Pharmaceutical Science</strong> with a minor in <strong>Computer Science</strong>. 
-            I love exploring new things and I am really passionate about music.
-          </div>
+        <div>
+          <Card
+            sx={{
+              width: "450px",
+              borderRadius: "10px",
+              height: "450px",
+              backgroundColor: "#efffe0",
+              display: "flex"
+            }}
+          >
+            <div className="card-contents">
+              <CardActionArea href="https://maryjess.github.io/ip/">
+                <CardMedia
+                  className="center"
+                  sx={{height: "200px", width: "450px", maxHeight: "200px", maxWidth: "450px"}}
+                  image={mew}
+                  title="Mew"
+                />
+              </CardActionArea>
+              <CardContent
+                sx={{padding: "30px", display: "flex"}}
+              >
+                <Stack justifyContent="space-evenly">
+                  <div>
+                    <a className="card" href="https://maryjess.github.io/ip/">
+                      <h3 className="card">Mew Chatbot</h3>
+                    </a>
+                    <p className="para">a simple task manager chatbot application written in Java.</p>
+                  </div>
+                  <CardActions>
+                    <Stack direction="row" spacing={2} sx={{alignSelf: "flex-end"}}>
+                      <Button 
+                        id="card-button"
+                        href="https://maryjess.github.io/ip/"
+                        sx={{fontFamily: "Montserrat", textTransform: "none"}} 
+                        variant="contained" 
+                        color="success" 
+                        size="small"
+                      >
+                        Learn More
+                      </Button>
+                      <Button 
+                        id="card-button"
+                        href="https://github.com/maryjess/ip"
+                        sx={{fontFamily: "Montserrat", textTransform: "none"}} 
+                        color="success" 
+                        variant="outlined"
+                        size="small"
+                      >
+                        Source Code
+                      </Button>
+                    </Stack>
+                  </CardActions>
+                </Stack>
+              </CardContent>
+            </div>
+          </Card>
+        </div>
+        <div className="padding-left">
+          <Card
+            sx={{
+              width: "450px",
+              borderRadius: "10px",
+              backgroundColor: "#efffe0",
+              height: "450px",
+              display: "flex"
+            }}
+          >
+            <div className="card-contents">
+              <CardActionArea href="https://github.com/yehudanathan/qr-bookmark">
+                <CardMedia
+                  className="center"
+                  sx={{height: "200px", width: "450px"}}
+                  image={qr_bookmark}
+                  title="QR-Bookmark"
+                />
+              </CardActionArea>
+              <CardContent
+                sx={{padding: "30px", display: "flex"}}
+              >
+                <Stack justifyContent="space-evenly">
+                  <div>
+                    <a className="card" href="https://github.com/yehudanathan/qr-bookmark">
+                      <h3 className="card">QR-Bookmark</h3>
+                    </a>
+                    <p className="para">a bookmark manager that can scan QR code for URLs.<br></br>
+                    <i>(Under construction)</i></p>
+                  </div>
+                  <CardActions>
+                    {/* <Stack direction="row" spacing={2} sx={{alignSelf: "flex-end"}}> */}
+                      {/* <Button 
+                        href="https://maryjess.github.io/ip/"
+                        sx={{fontFamily: "Montserrat", textTransform: "none"}} 
+                        variant="contained" 
+                        color="success" 
+                        size="small"
+                      >
+                        Learn More
+                      </Button> */}
+                      <Button
+                        id="card-button"
+                        href="https://github.com/yehudanathan/qr-bookmark"
+                        sx={{fontFamily: "Montserrat", textTransform: "none"}} 
+                        color="success" 
+                        variant="outlined"
+                        size="small"
+                      >
+                        Source Code
+                      </Button>
+                    {/* </Stack> */}
+                  </CardActions>
+                </Stack>
+              </CardContent>
+            </div>
+          </Card>
         </div>
       </Stack>
     </Stack>
